8c21a2cb3f4bb778d97c7715b5a4dd753837e1d6
Naman-Parlecha <namanparlecha@gmail.com>
2024-01-24 23:28:50 +0530

    Completed HomePage

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,25 +1,55 @@
+import React, { useState, useEffect } from "react";
 import "./Item.scss";
-export default function Item({img,price,name,desp}) {
+
+export default function Item({ img, price, name, desp }) {
+  const [quantity, setQuantity] = useState(1);
+
+  useEffect(() => {
+    setQuantity(1);
+  }, []);
+
+  function decrementHandler() {
+    if (quantity <= 1) {
+      return;
+    }
+    setQuantity((prevQuantity) => prevQuantity - 1);
+  }
+
+  function incrementHandler() {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  }
+
+  function ChangeHandler(e) {
+    const value = +e.target.value;
+    setQuantity(isNaN(value) ? 1 : value);
+  }
+
   return (
     <li>
-      <article>
-        <img src={img} alt="food image" />
-        <h2>{name}</h2>
-        <p className={"desp"}>
-          {desp}
-        </p>
-        <span>
-          <div className="pricing">
-            <p className="title">price</p>
-            <p>₹{price}</p>
-          </div>
+      <img src={img} alt="food image" />
+      <h2 className="title-name">{name}</h2>
+      <p className={"desp"}>{desp}</p>
+      <div className="details">
+        <div className="pricing">
+          <p className="price">
+            <span>price -</span>₹{price}
+          </p>
           <div className="quantity">
-          <p className="title">quantity</p>
-          <input type="number" defaultValue={1} />
+            <button onClick={decrementHandler}>-</button>
+            <input
+              className="input-number"
+              type="number"
+              value={quantity}
+              onChange={ChangeHandler}
+              inputMode="numeric"
+            />
+            <button onClick={incrementHandler}>+</button>
           </div>
-        </span>
-        <button>Add to cart</button>
-      </article>
+        </div>
+        <div className="add">
+          <button>Add to cart</button>
+        </div>
+      </div>
     </li>
   );
 }

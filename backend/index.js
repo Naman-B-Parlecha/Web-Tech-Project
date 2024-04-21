6debd0bd845c81eb6a5d6a3dff86186b6e13a131
Naman-Parlecha <namanparlecha@gmail.com>
2024-04-21 13:32:46 +0530

    cors changes

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://web-tech-project-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(dishesRoutes);
 
 app.use(express.urlencoded({ extended: true }));
